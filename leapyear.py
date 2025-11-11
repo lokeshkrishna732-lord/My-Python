7bec6a02d0b344ad717b4be6833720dad9b38201
lokeshkrishna732-lord <lokeshkrishna732@gmail.com>
2025-11-11 14:45:00 +0530

    Create leapyear.py

---
+++ leapyear.py
@@ -0,0 +1,9 @@
+#leap year
+year=int(input("enter the year: "))
+if year%4==0 and year%100!=0 or year%400==0:
+  print("leap year")
+else:
+  print("not a leap year")
+enter the year: 2024
+leap year
+
