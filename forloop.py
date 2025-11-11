a555f479cea87130055851a9c0ca62a06e93aa54
lokeshkrishna732-lord <lokeshkrishna732@gmail.com>
2025-11-11 14:54:02 +0530

    Create forloop.py

---
+++ forloop.py
@@ -0,0 +1,25 @@
+#for loop
+for a in range(1,20):
+ if a%2==0:
+  print("even number")
+ else:
+  print("odd number")
+   odd number
+even number
+odd number
+even number
+odd number
+even number
+odd number
+even number
+odd number
+even number
+odd number
+even number
+odd number
+even number
+odd number
+even number
+odd number
+even number
+odd number
