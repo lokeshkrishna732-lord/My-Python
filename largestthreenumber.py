6a5b55463866a6687a64b47e1ab95bb2b05d2706
lokeshkrishna732-lord <lokeshkrishna732@gmail.com>
2025-11-11 14:44:17 +0530

    Create largestthreenumber.py

---
+++ largestthreenumber.py
@@ -0,0 +1,14 @@
+#largest of three
+a=int(input("entee the number"))
+b=int(input("enter the number"))
+c=int(input("enter the number"))
+if a>=b and a>=c :
+  print("largestnumber is",a)
+elif b>=a and b>=c :
+  print("largest number is ",b)
+else:
+  print("latrgest number is",c)
+  enter the number15
+enter the number61
+enter the number22
+largest number is  61
