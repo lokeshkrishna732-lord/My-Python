9754e8176f9e66d34acb0eee69f34327bed90258
lokeshkrishna732-lord <lokeshkrishna732@gmail.com>
2025-11-11 14:47:30 +0530

    Create gradecalculation.py

---
+++ gradecalculation.py
@@ -0,0 +1,12 @@
+# grade calculation
+a= int(input("enter the number"))
+if a>=90:
+ print("gardeA")
+elif a>=75 :
+  print("gradeB")
+elif a>=50 :
+ print("gradeC")
+else:
+ print("fail")
+  enter the number50
+gradeC
