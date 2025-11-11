253b3aa5875b3cea5ac4bf361abade6987b68893
lokeshkrishna732-lord <lokeshkrishna732@gmail.com>
2025-11-11 14:42:55 +0530

    Create largesttwonumber.py

---
+++ largesttwonumber.py
@@ -0,0 +1,12 @@
+#largest of two
+a=int(input("enter the number"))
+b=int(input("enter the number"))
+if a>b:
+  print("largest number is",a)
+elif a<b:
+  print("largest numberis",b)
+else:
+  print("both are equal")
+  enter the number10
+enter the number15
+largest numberis 15
