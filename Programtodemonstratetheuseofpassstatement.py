e790b584215d679f8855540dd6c3cca4cc67bf38
lokeshkrishna732-lord <lokeshkrishna732@gmail.com>
2025-11-11 19:31:47 +0530

    Create Programtodemonstratetheuseofpassstatement.py

---
+++ Programtodemonstratetheuseofpassstatement.py
@@ -0,0 +1,16 @@
+# Program to demonstrate the use of pass statement
+
+for i in range(1, 11):
+    if i == 5:
+        pass  # does nothing, just a placeholder
+    else:
+        print(i)m
+      1
+2
+3
+4
+6
+7
+8
+9
+10
