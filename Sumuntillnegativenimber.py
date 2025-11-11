ebb20e0eb832db6bb6aad05e982017072b1c2eac
lokeshkrishna732-lord <lokeshkrishna732@gmail.com>
2025-11-11 19:30:43 +0530

    Create Sumuntillnegativenimber.py

---
+++ Sumuntillnegativenimber.py
@@ -0,0 +1,22 @@
+#sum until negative number
+total=0
+while True:
+  num=int(input("enter a number"))
+  if num<0:5
+    break
+  total+=num
+print("sum of entered numbers:",total)
+enter a number10
+enter a number15
+enter a number25
+enter a number60
+enter a number47
+enter a number32
+enter a number56
+enter a number45
+enter a number65
+enter a number565
+enter a number454
+enter a number4
+enter a number6
+
