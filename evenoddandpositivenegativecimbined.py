7bc06514e69a01c0fa0b7ceff8423c581eef7456
lokeshkrishna732-lord <lokeshkrishna732@gmail.com>
2025-11-11 14:53:25 +0530

    Create evenoddandpositivenegativecimbined.py

---
+++ evenoddandpositivenegativecimbined.py
@@ -0,0 +1,14 @@
+#even/odd and positive/negatuve combined.
+num=int(input("enter the number"))
+if num%2==0 and num>0 :
+  print("even and positive")
+elif num%2==0 and num<0 :
+  print("even and negative")
+elif num%2!=0 and num>0 :
+  print("odd and positive")
+elif num%2!=0 and num<0:
+  print("odd and negative")
+else:
+  print("the number is zero")
+  enter the number-42
+even and negative
