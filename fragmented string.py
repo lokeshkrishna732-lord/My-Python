5cc101a97aeaf1e94c223c442a90ce356e46d71d
lokeshkrishna732-lord <lokeshkrishna732@gmail.com>
2025-11-12 12:00:52 +0530

    Create fragmented string.py

---
+++ fragmented string.py
@@ -0,0 +1,7 @@
+#fragmented string
+name="lord"
+age=18
+college="siet"
+department="aiml"
+print(f"my name is: {name} i am {age} old and I am studying in: {college} in department {department}")
+my name is: lord i am 18 old and I am studying in: siet in department aiml
