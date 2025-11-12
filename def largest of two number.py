e5e0b571f08543feca04e85a7ebbe31a3c85a39e
lokeshkrishna732-lord <lokeshkrishna732@gmail.com>
2025-11-12 22:17:12 +0530

    Create def largest of two number.py

---
+++ def largest of two number.py
@@ -0,0 +1,8 @@
+#largest of two number
+def largest_number(a,b):
+  if a>b:
+   print(f"{a} is greater than {b}")
+  else:
+   print(f"{b} is greater than {a}")
+largest_number(2,3)
+3 is greater than 2
