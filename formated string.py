6a0ad5e32b9015f4a8813a53ecb6658dd69ac0e2
lokeshkrishna732-lord <lokeshkrishna732@gmail.com>
2025-11-12 10:26:30 +0530

    Create formated string.py

---
+++ formated string.py
@@ -0,0 +1,6 @@
+name="lord"
+age=18
+college="siet"
+department="aiml"
+print(f"my name is: {name} i am {age} old and I am studying in: {college} in department {department}")
+my name is: lord i am 18 old and I am studying in: siet in department aiml
