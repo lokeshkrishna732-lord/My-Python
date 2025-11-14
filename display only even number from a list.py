104b29eeae0943b3fdb979e35dbf9ba0f3303e37
lokeshkrishna732-lord <lokeshkrishna732@gmail.com>
2025-11-14 12:36:02 +0530

    Create display only even number from a list.py

---
+++ display only even number from a list.py
@@ -0,0 +1,5 @@
+#display only evennumber from a list
+a = [1,2,3,4,5]
+even_numbers = [num for num in a if num % 2 == 0]
+print(f"The even numbers in the list are: {even_numbers}")
+The even numbers in the list are: [2, 4]
