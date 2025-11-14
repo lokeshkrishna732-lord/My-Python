8b580b5df6567b0a6014175367fc99083d1dc9f1
lokeshkrishna732-lord <lokeshkrishna732@gmail.com>
2025-11-14 12:31:32 +0530

    Add min and max calculation script
    Implement a script to find max and min values in a list.

---
+++ min and max.py
@@ -0,0 +1,8 @@
+#max and min of elements
+a = [1, 2, 3, 4, 5]
+max_value = max(a)
+min_value = min(a)
+print(f"The maximum value in the list is: {max_value}")
+print(f"The minimum value in the list is: {min_value}")
+The maximum value in the list is: 5
+The minimum value in the list is: 1
