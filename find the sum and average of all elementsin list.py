e87392b63e85ae7abb97d98a4f4b34a30c03eda9
lokeshkrishna732-lord <lokeshkrishna732@gmail.com>
2025-11-14 12:30:41 +0530

    Create find the sum and average of all elementsin list.py

---
+++ find the sum and average of all elementsin list.py
@@ -0,0 +1,8 @@
+# find the sum and average of all element in list.
+a = [1, 2, 3, 4, 5]
+total_sum = sum(a)
+average = total_sum / len(a)
+print(f"The sum of the elements is: {total_sum}")
+print(f"The average of the elements is: {average}")
+The sum of the elements is: 15
+The average of the elements is: 3.0
