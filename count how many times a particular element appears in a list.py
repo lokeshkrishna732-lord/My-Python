bd9f817d6685f372e731146a276f20952ab6f983
lokeshkrishna732-lord <lokeshkrishna732@gmail.com>
2025-11-14 12:33:17 +0530

    Create count how many times a particular element appears in a list.py

---
+++ count how many times a particular element appears in a list.py
@@ -0,0 +1,7 @@
+#count how many times a particular element appears in a list
+a = [1, 2, 3, 4, 5, 2, 3, 2]
+element_to_count = 2
+count = a.count(element_to_count)
+print(f"The element {element_to_count} appears {count} times in the list.")
+The element 2 appears 3 times in the list.
+
