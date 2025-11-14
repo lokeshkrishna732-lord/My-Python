818de9f228c40d141a47ebcb131ba3b7fec89026
lokeshkrishna732-lord <lokeshkrishna732@gmail.com>
2025-11-14 12:28:48 +0530

    Print the list of numbers entered by the user

---
+++ list of 5 num by user and print the list.py
@@ -0,0 +1,12 @@
+#list of 5 numbers entered by user and print the list
+numbers = []
+for i in range(5):
+    number = int(input("Enter a number: "))
+    numbers.append(number)
+print("The list of numbers is:", numbers)
+Enter a number: 10
+Enter a number: 25
+Enter a number: 4
+Enter a number: 6
+Enter a number: 641
+The list of numbers is: [10, 25, 4, 6, 641]
