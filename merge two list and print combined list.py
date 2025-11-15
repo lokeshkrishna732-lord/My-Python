f65ce0118aa64538fb327d68fbfcd611107de5b5
lokeshkrishna732-lord <lokeshkrishna732@gmail.com>
2025-11-15 09:29:51 +0530

    Create merge two list and print combined list.py

---
+++ merge two list and print combined list.py
@@ -0,0 +1,6 @@
+#merge two lists and print combined list
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+combined_list = list1 + list2
+print(f"The combined list is: {combined_list}")
+The combined list is: [1, 2, 3, 4, 5, 6]
