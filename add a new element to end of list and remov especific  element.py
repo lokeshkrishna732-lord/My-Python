abe774862a5dd6f89f3eac466a6bec8d993cfd22
lokeshkrishna732-lord <lokeshkrishna732@gmail.com>
2025-11-15 09:25:47 +0530

    Create add a new element to end of list and remov especific  element.py

---
+++ add a new element to end of list and remov especific  element.py
@@ -0,0 +1,6 @@
+#add a new element to end of lidt an d remove specific element
+a = [1, 2, 3, 4, 5]
+new_element = 6
+a.append(new_element)
+print(f"The updated list after adding {new_element} is: {a}")
+The updated list after adding 6 is: [1, 2, 3, 4, 5, 6]
