4e7636fb01919fca18738f977353d09c388366d4
lokeshkrishna732-lord <lokeshkrishna732@gmail.com>
2025-11-15 09:26:40 +0530

    Create sort list i asending and desending order.py

---
+++ sort list i asending and desending order.py
@@ -0,0 +1,7 @@
+#Sort list in ascending and desending oreder
+a = [5, 2, 8, 1, 9]
+a.sort
+print(f"The sorted list in ascending order is: {a}")
+print(f"The sorted list in desendind oreder is: {a}")
+The sorted list in ascending order is: [5, 2, 8, 1, 9]
+The sorted list in desendind oreder is: [5, 2, 8, 1, 9]
