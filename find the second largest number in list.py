aed1235a7922aa994ec54dd59fe0bdd914f94406
lokeshkrishna732-lord <lokeshkrishna732@gmail.com>
2025-11-15 09:28:02 +0530

    Create find the second largest number in list.py

---
+++ find the second largest number in list.py
@@ -0,0 +1,6 @@
+#find the second largest in a list
+a = [5, 2, 8, 1, 9]
+a.sort()
+second_largest = a[-2]
+print(f"The second largest number in the list is: {second_largest}")
+The second largest number in the list is: 8
